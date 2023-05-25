100ec2a8127bcb22cbed6fde78af84f5fb19e956
kau14376 <kau14376@sheridancollege.ca>
2023-05-25 15:26:38 -0400

    branch v1

--- src/week3example/Week3Example.java
+++ src/week3example/Week3Example.java
@@ -13,6 +13,8 @@ public class Week3Example {
     /*push steps 1. initilize versioning, .gitconfig file, .gitignore, local repository, commit */
     public static void main(String[] args) {
         System.out.println("learning github operations");
+        System.out.println("learning github pull");
+        // this is the branch
     }
     
 }
