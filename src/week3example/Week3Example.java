750578af14323e0d849ec68d891e3f9b4948d03c
kau14376 <kau14376@sheridancollege.ca>
2023-05-25 14:31:50 -0400

    Week3Example

---
+++ src/week3example/Week3Example.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package week3example;
+
+/**
+ *
+ * @author kaurt
+ */
+public class Week3Example {
+
+    /*push steps 1. initilize versioning, .gitconfig file, .gitignore, local repository, commit */
+    public static void main(String[] args) {
+        System.out.println("learning github operations");
+    }
+    
+}
